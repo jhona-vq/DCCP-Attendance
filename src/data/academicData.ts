1415ebab2288037d9a51d20dd654ab847775bd94
Quines Jhona <jhonav402@gmail.com>
2025-04-28 12:30:17 +0800

    Update academicData.ts

--- src/data/academicData.ts
+++ src/data/academicData.ts
@@ -5,17 +5,8 @@ export const mockSubjects: Subject[] = [
   // BSIT Subjects
   {
     id: 'bsit101',
-    code: 'IT 101',
-    name: 'Introduction to Computing',
-    units: 3,
-    program: 'BSIT',
-    yearLevel: '1st Year',
-    semester: '1st Semester'
-  },
-  {
-    id: 'bsit102',
-    code: 'IT 102',
-    name: 'Computer Programming 1',
+    code: 'GE-1',
+    name: 'Understanding the Self',
     units: 3,
     program: 'BSIT',
     yearLevel: '1st Year',
@@ -23,13 +14,563 @@ export const mockSubjects: Subject[] = [
   },
   {
     id: 'bsit103',
-    code: 'IT 103',
-    name: 'Computer Programming 2',
+    code: 'GE-2',
+    name: 'Readings in the Philippine History',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit104',
+    code: 'GE-3',
+    name: 'The Contemporary World',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit105',
+    code: 'GE-4',
+    name: 'Art Appreciation',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit106',
+    code: 'GE-5',
+    name: 'Mathematics in the Modern World',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit107',
+    code: 'GE-6',
+    name: 'Science,Technology, and Society',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit108',
+    code: 'ITW 101',
+    name: 'Introduction to Computing',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit109',
+    code: 'PE 1',
+    name: 'Self-Testing Activities',
+    units: 2,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit109',
+    code: 'NSTP 1',
+    name: 'Civic Welfare Training Service 1',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit110',
+    code: 'GE-U7',
+    name: 'Purposive Communication',
     units: 3,
     program: 'BSIT',
     yearLevel: '1st Year',
     semester: '2nd Semester'
   },
+  {
+    id: 'bsit111',
+    code: 'GE-8',
+    name: 'Ethics',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit112',
+    code: 'GE-9',
+    name: 'Rizals Life and Works',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit113',
+    code: 'GE-10',
+    name: 'Living in the IT Era',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit114',
+    code: 'GE-11',
+    name: 'Philippine Popular Culture',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit115',
+    code: 'GE-12',
+    name: 'The Entrepreneurial Mind',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit116',
+    code: 'ITW 102',
+    name: 'Computer Programming-1 (Java)',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit117',
+    code: 'PE-2',
+    name: 'Individual Sports',
+    units: 2,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit118',
+    code: 'NSTP 2',
+    name: 'Civil Welfare Training Service 2',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit119',
+    code: 'NatSci 221',
+    name: 'General Science (Chemistry and Biology)',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit120',
+    code: 'Human 211',
+    name: 'Philosophy (Logic)',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '1st Semester'
+  },
+   {
+    id: 'bsit121',
+    code: 'CTyp 201',
+    name: 'Computer Typing 1',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '1st Semester'
+  },
+   {
+    id: 'bsit122',
+    code: 'ITW 211',
+    name: 'Computer Programming 2(C#.Net)',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '1st Semester'
+  },
+   {
+    id: 'bsit123',
+    code: 'ITW 212',
+    name: 'Computer Architecture and Organization with Assembly',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '1st Semester'
+  },
+   {
+    id: 'bsit124',
+    code: 'ITW 213',
+    name: 'Graphics Designing (Photoshop)',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '1st Semester'
+  },
+   {
+    id: 'bsit125',
+    code: 'ITW 214',
+    name: 'Discrete Mathematics',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '1st Semester'
+  },
+   {
+    id: 'bsit126',
+    code: 'NatSci 222',
+    name: 'Phisics 1',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '1st Semester'
+  },
+   {
+    id: 'bsit127',
+    code: 'Fil 1',
+    name: 'Kontekstwalisadong Komunikasyon sa Filipino',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '1st Semester'
+  },
+   {
+    id: 'bsit128',
+    code: 'PE 3',
+    name: 'RHythmic Activities',
+    units: 2,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '1st Semester'
+  },
+   {
+    id: 'bsit129',
+    code: 'SocSci 221',
+    name: 'General Psychology',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit130',
+    code: 'SocSci 222',
+    name: 'Principles of Economics with TAR',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit131',
+    code: 'ITW 221',
+    name: 'Data Structure and Algorithm Analysis',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit132',
+    code: 'ITW 222',
+    name: 'Layouting (AdobeInDesign)',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit133',
+    code: 'ITW 223',
+    name: 'Object Oriented Programming',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit134',
+    code: 'ITW 224',
+    name: 'Computer Programming 2 (Python)',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit135',
+    code: 'ITW 225',
+    name: 'Operating System Concepts',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit136',
+    code: 'ITW 226',
+    name: 'Digital Circuits',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit137',
+    code: 'Fil 2',
+    name: 'Filipino sa Ibat-ibang Disiplina',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit138',
+    code: 'PE 4',
+    name: 'Team Sports',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '2nd Year',
+    semester: '2nd Semester'
+  },
+  {
+    id: 'bsit139',
+    code: 'Acctg 301',
+    name: 'Fundamentals of Accounting',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '3rd Year',
+    semester: '1st Semester'
+  },
+   {
+    id: 'bsit140',
+    code: 'SocSci 322',
+    name: 'Sociology w/ Pop. Ed. & Drug Prevention',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '3rd Year',
+    semester: '1st Semester'
+  },
+   {
+    id: 'bsit141',
+    code: 'ITW 311',
+    name: 'Information Management (ER/SQL)',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '1st Year',
+    semester: '1st Semester'
+  },
+   {
+    id: 'bsit142',
+    code: 'ITW 312',
+    name: 'Computer Programming 4 (Web Programming & Design)',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '3rd Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit143',
+    code: 'ITW 313',
+    name: 'Hardware and Software Installation',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '3rd Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit144',
+    code: 'ITW 314',
+    name: 'Data Communications and Networking',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '3rd Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit145',
+    code: 'ITW 315',
+    name: 'Management Information System',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '3rd Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit146',
+    code: 'ITW 316',
+    name: 'Software Engineering',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '3rd Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit147',
+    code: 'ITW 321',
+    name: 'System Analysis and Design',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '3rd Year',
+    semester: '2nd Semester'
+  },
+   {
+    id: 'bsit148',
+    code: 'ITW 322',
+    name: 'Linux Operating System',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '3rd Year',
+    semester: '2nd Semester'
+  },
+   {
+    id: 'bsit149',
+    code: 'ITW 323',
+    name: 'Computer Programming 5 (Web Design and Development)',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '3rd Year',
+    semester: '2nd Semester'
+  },
+   {
+    id: 'bsit150',
+    code: 'ITW 324',
+    name: 'Systems Administration and Maintainance',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '3rd Year',
+    semester: '2nd Semester'
+  },
+   {
+    id: 'bsit151',
+    code: 'ITW 325',
+    name: 'Fundamentals of Business Analytics',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '3rd Year',
+    semester: '2nd Semester'
+  },
+   {
+    id: 'bsit152',
+    code: 'ITW 326',
+    name: 'Information Assurance and Security',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '3rd Year',
+    semester: '2nd Semester'
+  },
+   {
+    id: 'bsit153',
+    code: 'ITW 327',
+    name: 'Project Management',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '3rd Year',
+    semester: '2nd Semester'
+  },
+   {
+    id: 'bsit154',
+    code: 'ITW 328',
+    name: 'Professional Ethics in IT and Comp. Issues',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '3rd Year',
+    semester: '2nd Semester'
+  },
+   {
+    id: 'bsit155',
+    code: 'ITW 411',
+    name: 'Capstone Project -1',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '4th Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit156',
+    code: 'ITW 412',
+    name: 'Mobile Technology',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '4th Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit157',
+    code: 'ITW Elec1',
+    name: 'Integrative Programming Technologies',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '4th Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit158',
+    code: 'ITW Elec2',
+    name: 'Systems Integration and Architecture',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '4th Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit159',
+    code: 'ITW Elec3',
+    name: 'Special Topics in IT',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '4th Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit160',
+    code: 'ITW 413',
+    name: 'Seminars and Workshops',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '4th Year',
+    semester: '1st Semester'
+  },
+  {
+    id: 'bsit161',
+    code: 'ITW 421',
+    name: 'Capstone-2',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '4th Year',
+    semester: '2nd Semester'
+  },
+   {
+    id: 'bsit162',
+    code: 'ITW Elec4 ',
+    name: 'Human Computer Interaction',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '4th Year',
+    semester: '2nd Semester'
+  },
+   {
+    id: 'bsit163',
+    code: 'ITW Elec5',
+    name: 'Web Enhanced Animation Graphics',
+    units: 3,
+    program: 'BSIT',
+    yearLevel: '4th Year',
+    semester: '2nd Semester'
+  },
+   
   
   // BSHM Subjects
   {
